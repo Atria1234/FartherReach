a171ebda6daf7e679dad582b6c8db43f14a6397b
Tomáš Chmelík <tomas.chmelik2@gmail.com>
2023-08-05 06:48:11 +0200

    1.0.0

---
+++ settings.lua
@@ -0,0 +1,4 @@
+data.raw["int-setting"]["far-reach-build-distance-bonus"].maximum_value = nil
+data.raw["int-setting"]["far-reach-reach-distance-bonus"].maximum_value = nil
+data.raw["int-setting"]["far-reach-resource-reach-distance-bonus"].maximum_value = nil
+data.raw["int-setting"]["far-reach-item-drop-distance-bonus"].maximum_value = nil
